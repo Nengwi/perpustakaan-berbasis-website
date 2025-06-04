b9476fbc41616b49324fc47340363c5de9abb94f
nengwi <nengmilaningrum@gmail.com>
2025-06-04 12:03:08 +0700

    Menampilkan daftar semua buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -7,17 +7,21 @@ use Illuminate\Http\Request;
 
 class BukuController extends Controller
 {
+ //Menampilkan daftar semua buku
     public function index()
     {
         $bukus = Buku::all();
         return view('buku.index', compact('bukus'));
     }
 
+   //Menampilkan form tambah buku
     public function create()
     {
         return view('buku.create');
     }
 
+  //Menyimpan data buku dari form
+//Validasi dulu, baru simpan ke DB
     public function store(Request $request)
     {
         $request->validate([
@@ -33,11 +37,14 @@ class BukuController extends Controller
         return redirect()->route('buku.index')->with('success', 'Buku berhasil ditambahkan.');
     }
 
+    //Menampilkan form edit buku
+ //Otomatis ambil buku berdasarkan ID
     public function edit(Buku $buku)
     {
         return view('buku.edit', compact('buku'));
     }
 
+    //Menyimpan hasil edit ..Validasi, lalu update ke DB, Redirect ke daftar buku
     public function update(Request $request, Buku $buku)
     {
         $request->validate([
@@ -53,6 +60,7 @@ class BukuController extends Controller
         return redirect()->route('buku.index')->with('success', 'Buku berhasil diperbarui.');
     }
 
+   //Menghapus buku dari database, Redirect ke daftar buku + pesan sukses
     public function destroy(Buku $buku)
     {
         $buku->delete();
